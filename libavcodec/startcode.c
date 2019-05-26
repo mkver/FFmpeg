1d9248661c8fe34e0d0e759415866c8547aa2778
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-05-26 15:17:55 +0200

    avcodec/startcode: Don't return for every zero
    Until now the function ff_startcode_find_candidate_c did not really
    search for startcodes (the startcode 0x00 0x00 0x01 (used in
    MPEG-1/2/4, VC-1 and H.264/5) is the only startcode meant here). Instead
    it searched for zero bytes and returned the earliest position of a zero
    byte. This of course led to lots of false positives - millions per GB
    of video.
    This has been changed: The first position of the buffer that
    may be part of a four-byte startcode is now returned. This excludes zero
    bytes that are known not to belong to a startcode, but zero bytes at the
    end of a buffer that might be part of a startcode whose second part is in
    the next buffer are also returned. This is in accordance with the
    expectations of the current callers of ff_startcode_find_candidate_c,
    namely the H.264 parser and the VC-1 parser.
    Getting rid of lots of function calls with its accompanying overhead of
    course brings a little speed-up with it.

--- libavcodec/startcode.c
+++ libavcodec/startcode.c
@@ -22,51 +22,159 @@
  * @file
  * Accelerated start code search function for start codes common to
  * MPEG-1/2/4 video, VC-1, H.264/5
- * @author Michael Niedermayer <michaelni@gmx.at>
+ * @author Michael Niedermayer <michaelni@gmx.at>,
+ * @author Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
  */
 
 #include "startcode.h"
 #include "config.h"
+#include "libavutil/common.h"
 #include "libavutil/intreadwrite.h"
 
 int ff_startcode_find_candidate_c(const uint8_t *buf, int size)
 {
     const uint8_t *start = buf, *end = buf + size;
 
+    /* The MAIN_LOOP tries to read several bytes at once.
+     * A startcode's 0x00 0x01 or 0x00 0x00 will be detected
+     * by it if these bytes are contained within the bytes
+     * read at once.
+     * It will always be made sure that at the beginning of
+     * every iteration of the loop buf points at most to the
+     * next possible position of a 0x00 immediately preceding
+     * a startcode's 0x01. */
 #define MAIN_LOOP(bitness, mask1, mask2) do {                              \
-        for (; buf <= end - bitness / 8; buf += bitness / 8)               \
-            if ((~READ(bitness)(buf) & (READ(bitness)(buf) - mask1))       \
-                                     & mask2)                              \
-                break;                                                     \
+        for (; buf <= end - bitness / 8; ) {                               \
+            if (!((~READ(bitness)(buf) & (READ(bitness)(buf) - mask1))     \
+                                           & mask2)) {                     \
+                buf += bitness / 8;                                        \
+                continue;                                                  \
+            }                                                              \
+            /* No bounds check is necessary here as the bytes
+             * just read are known to contain a zero. */                   \
+            while (*buf)                                                   \
+                buf++;                                                     \
+            /* Go to the first nonzero. */                                 \
+            do {                                                           \
+                if (END_CHECK(bitness / 8))                                \
+                    goto reached_end;                                      \
+            } while (!*buf);                                               \
+            STARTCODE_AND_ALIGNMENT_CHECK(bitness / 8);                    \
+        }                                                                  \
+        /* buf points at most to the earliest possible position of a 0x00
+         * preceding a startcode's 0x01. The checks used at the end detect
+         * whether *buf is a startcode's 0x01, so we may increment buf.
+         * This also ensures that buf >= start + 2. */                     \
+        buf++;                                                             \
     } while (0)
 
+    /* If the 0x01 of a startcode is at position buf,
+     * the following check detects it. Requires buf >= start + 2.
+     * After the check buf points to the earliest possible
+     * position of the next startcode's 0x01 (if no startcode
+     * was found) and the check can be reapplied. */
+#define STARTCODE_CHECK do {                                               \
+        if (*buf > 1) {                                                    \
+            buf += 3;                                                      \
+        } else if (buf[-1]) {                                              \
+            buf += 2;                                                      \
+        } else if (buf[-2] || *buf == 0) {                                 \
+            buf += 1;                                                      \
+        } else                                                             \
+            goto found_startcode;                                          \
+    } while (0)
 
 #if HAVE_FAST_UNALIGNED
 #define READ(bitness) AV_RN ## bitness
+#define END_CHECK(mod) (++buf == end)
+    /* In contrast to the above STARTCODE_CHECK, this check
+     * jumps over at most a startcode's initial 0x00
+     * in accordance with the requirements of MAIN_LOOP. */
+#define STARTCODE_AND_ALIGNMENT_CHECK(bitness) do {                        \
+        if (*buf > 1) {                                                    \
+            buf += 2;                                                      \
+        } else if (buf[-2] == 0)                                           \
+            goto found_startcode;                                          \
+    } while (0)
+
+    /* It is allowed to jump over a startcode's first 0x00.
+     * Doing so here ensures that the requirement buf >= start + 2
+     * of the above check is always fulfilled, because buf
+     * also gets incremented in MAIN_LOOP, too. */
+    buf++;
 
 #if HAVE_FAST_64BIT
     MAIN_LOOP(64, 0x0101010101010101ULL, 0x8080808080808080ULL);
 #else
     MAIN_LOOP(32, 0x01010101U, 0x80808080U);
 #endif
+
+    while (buf < end)
+        STARTCODE_CHECK;
+
+reached_end:
+
 #else
 #define READ(bitness) AV_RN ## bitness ## A
-#define ALIGNMENT_CHECK(bitness) do { \
-    for (; buf < end && (uintptr_t)buf % (bitness / 8); buf++) \
-        if (!*buf) \
-            return buf - start; \
-    } while (0) \
+    /* In unaligned mode, we require that there is enough data
+     * available so that MAKE_ALIGNED(++buf) is always contained
+     * in the buffer. This implies that we do not overread
+     * during STARTCODE_AND_ALIGNMENT_CHECK. */
+#define END_CHECK(mod) (++buf > end - (mod))
+#define MAKE_ALIGNED(mod) \
+    ((const uint8_t *)((uintptr_t)(buf + (mod) - 1) / (mod) * (mod)))
+#define STARTCODE_AND_ALIGNMENT_CHECK(mod) do {                            \
+        make_aligned:                                                      \
+            temp = MAKE_ALIGNED(mod);                                      \
+        startcode_check:                                                   \
+            do {                                                           \
+                STARTCODE_CHECK;                                           \
+            } while (buf <= temp);                                         \
+            buf = temp;                                                    \
+    } while (0)
+
+    /* Given that the unaligned case requires special initialization
+     * dependent on the bitness, another macro is used. */
+#define ALIGNED(bitness, mask1, mask2) do {                                \
+        const uint8_t *temp;                                               \
+        buf += 2;                                                          \
+        if (buf + bitness / 8 > end)                                       \
+            goto reached_end;                                              \
+        goto make_aligned;                                                 \
+                                                                           \
+        MAIN_LOOP(bitness, mask1, mask2);                                  \
+                                                                           \
+    reached_end:                                                           \
+        if (buf < end) {                                                   \
+            temp = end - 1;                                                \
+            goto startcode_check;                                          \
+        }                                                                  \
+    } while (0)
 
 #if HAVE_FAST_64BIT
-    ALIGNMENT_CHECK(64);
-    MAIN_LOOP(64, 0x0101010101010101ULL, 0x8080808080808080ULL);
+    ALIGNED(64, 0x0101010101010101ULL, 0x8080808080808080ULL);
 #else
-    ALIGNMENT_CHECK(32);
-    MAIN_LOOP(32, 0x01010101U, 0x80808080U);
+    ALIGNED(32, 0x01010101U, 0x80808080U);
 #endif
 #endif
-    for (; buf < end; buf++)
-        if (!*buf)
+
+    /* No startcode found, but if the last bytes vanish,
+     * they may be part of a startcode whose end is not
+     * in the current buffer. Return the offset of the
+     * earliest element that may belong to a startcode. */
+    for (buf = end; buf > FFMAX(start, end - 3); buf--)
+        if (buf[-1])
             break;
+
+    return buf - start;
+
+found_startcode:
+    /* buf points to a startcode's 0x01. We have to
+     * check whether it is a four-byte startcode. */
+
+    buf -= 2;
+    if (buf > start && buf[-1] == 0)
+        buf--;
+
     return buf - start;
 }
