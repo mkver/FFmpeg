f642b4c23a4c179876e59879965d882982646fde
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-06-04 02:53:00 +0200

    startcode: Switch to index

--- libavcodec/startcode.c
+++ libavcodec/startcode.c
@@ -29,37 +29,34 @@
 #include "startcode.h"
 #include "config.h"
 #include "libavutil/intreadwrite.h"
-#include "libavutil/macros.h"
+#include "libavutil/common.h"
 
 int ff_startcode_find_candidate_c(const uint8_t *buf, int size)
 {
-    const uint8_t *start = buf, *end = buf + size, *temp;
+    int i = 1, temp;
 
 #define INITIALIZATION(mod) do {                                           \
-        if (end - start <= mod + 1)                                        \
+        if (size <= mod + 1)                                               \
             goto near_end;                                                 \
         /* From this point on until the end of the MAIN_LOOP,              \
          * buf is the earliest possible position of a 0x00                 \
          * immediately preceding a startcode's 0x01, i.e.                  \
          * everything from start to buf (inclusive) is known               \
          * to not contain a startcode's 0x01. */                           \
-        buf += 1;                                                          \
-        temp = (const uint8_t *)FFALIGN((uintptr_t)buf, mod);              \
-        /* Effective end for MAIN_LOOP in order not to overread. */        \
-        end -= mod + 1;                                                    \
+        temp = (const uint8_t *)((uintptr_t)(buf + i + mod - 1) / mod * mod) - buf;  \
+        size-= mod;                                                        \
         goto startcode_check;                                              \
     } while (0)
 
 #define READ(bitness) AV_RN ## bitness ## A
-#define STARTCODE_CHECK(offset) do {                                       \
-        if (buf[2 - offset] > 1) {                                         \
-            buf += 3;                                                      \
-        } else if (buf[1 - offset]) {                                      \
-            buf += 2;                                                      \
-        } else if (buf[-offset] || buf[2 - offset] == 0) {                 \
-            buf += 1;                                                      \
+#define STARTCODE_CHECK do { \
+        if (buf[i + 1] > 1) {                                              \
+            i += 3;                                                        \
+        } else if (buf[i]) {                                               \
+            i += 2;                                                        \
+        } else if (buf[i - 1] || buf[i + 1] == 0) {                        \
+            i += 1;                                                        \
         } else {                                                           \
-            buf += 1 - offset;                                             \
             goto found_startcode;                                          \
         }                                                                  \
     } while (0)
@@ -69,28 +66,30 @@ int ff_startcode_find_candidate_c(const uint8_t *buf, int size)
      * by it if these bytes are contained within the bytes
      * read at once. */
 #define MAIN_LOOP(bitness, mask1, mask2) do {                              \
-        for (; buf < end; ) {                                              \
-            if (!((~READ(bitness)(buf) & (READ(bitness)(buf) - mask1))     \
-                                       &  mask2)) {                        \
-                buf += bitness / 8;                                        \
+        for (; i < size ; ) {                                              \
+            if (!((~READ(bitness)(buf + i) & (READ(bitness)(buf + i) - mask1))\
+                                           &  mask2)) {                    \
+                i += bitness / 8;                                          \
                 continue;                                                  \
             }                                                              \
-            temp = buf + bitness / 8;                                      \
-            /* If the 0x01 of a startcode is at position buf + 1,          \
+            temp = i + bitness / 8;                                        \
+            /* If the 0x01 of a startcode is at position buf + i + 1,      \
              * the following check detects it.                             \
              * Because buf got incremented before entering this            \
              * loop, buf - 1 may be evaluated.                             \
              * Because temp + 1 < start + size, buf is always              \
              * <= the end of the buffer during the closing check           \
-             * so that the pointer comparison is defined. */               \
+             * so that the pointer comparison is defined.                  \
+             * Because i gets initialized to 1,                            \
+             * buf + i - 1 may be evaluated. */                            \
         startcode_check:                                                   \
             do {                                                           \
-                STARTCODE_CHECK(1);                                        \
-            } while (buf < temp);                                          \
-            buf = temp;                                                    \
+                STARTCODE_CHECK;                                           \
+            } while (i < temp);                                            \
+            i = temp;                                                      \
         }                                                                  \
-        /* Revert to the real end. */                                      \
-        end += bitness / 8 + 1;                                            \
+        /* Revert to the real size. */                                     \
+        size += bitness / 8;                                               \
     } while (0)
 
 #if HAVE_FAST_64BIT
@@ -109,42 +108,30 @@ int ff_startcode_find_candidate_c(const uint8_t *buf, int size)
 #endif
 #endif
 
-    /* For the end, buf is the earliest possible position
-     * of a three-byte startcode's leading 0x00. This is
-     * done in order not to run into the undefined behaviour
-     * explained below. */
-    buf--;
 near_end:
-    while (2 < end - buf) {
-        /* The STARTCODE_CHECK in the MAIN_LOOP can't be used
-         * to check whether the file ends with a startcode,
-         * because for this buf would need to be end - 2.
-         * But depending on the value of end[-1], buf might get
-         * incremented by 3 and therefore be beyond end. But
-         * pointer arithmetic beyond end is undefined behaviour.
-         * So a STARTCODE_CHECK with a different offset is used
-         * here: It detects whether buf points to the first 0x00
-         * of a three-byte startcode. */
-        STARTCODE_CHECK(0);
+    if (i < size - 1) {
+        temp = size - 1;
+        size-= 8;
+        goto startcode_check;
     }
 
     /* No startcode found, but if the last bytes vanish,
      * they may be part of a startcode whose end is not
      * in the current buffer. Return the offset of the
      * earliest element that may belong to a startcode. */
-    for (buf = end; buf > (end - start > 3 ? end - 3 : start); buf--)
-        if (buf[-1])
+    for (i = size; i > FFMAX(size - 3, 0); i--)
+        if (buf[i - 1])
             break;
 
-    return buf - start;
+    return i;
 
 found_startcode:
-    /* buf points to the byte preceding a startcode's 0x01.
+    /* buf + i points to the byte preceding a startcode's 0x01.
      * Check whether it is a four-byte startcode. */
 
-    buf -= 1;
-    if (buf > start && buf[-1] == 0)
-        buf--;
+    i -= 1;
+    if (i > 0 && buf[i - 1] == 0)
+        i--;
 
-    return buf - start;
+    return i;
 }
