f5ed10893e3e599d72e6e046b816fa2d952b2ba6
Andreas Rheinhardt <andreas.rheinhardt@gmail.com>
2019-07-25 23:39:15 +0200

    h264_metadata: Add option to minimize idr_pic_id
    Using values other than 0 or 1 for idr_pic_id is a waste of bits
    that happens to exist in the wild. Therefore a new option has been added
    to h264_metadata to minimize idr_pic_id; it is enabled by default.

--- libavcodec/h264_metadata_bsf.c
+++ libavcodec/h264_metadata_bsf.c
@@ -85,6 +85,8 @@ typedef struct H264MetadataContext {
     double rotate;
     int flip;
 
+    int idr;
+
     int level;
 } H264MetadataContext;
 
@@ -516,6 +518,23 @@ static int h264_metadata_filter(AVBSFContext *bsf, AVPacket *pkt)
         }
     }
 
+    if (ctx->idr != 2) {
+        int idr_access = h264_in->last_slice_nal_unit_type == H264_NAL_IDR_SLICE;
+
+        for (i = 0; i < au->nb_units; i++) {
+            CodedBitstreamUnit    *unit = &au->units[i];
+            CodedBitstreamUnitType type = unit->type;
+
+            if (type == H264_NAL_IDR_SLICE ||
+                type == H264_NAL_AUXILIARY_SLICE && idr_access) {
+                H264RawSliceHeader *slice = unit->content;
+                slice->idr_pic_id = idr_access & ctx->idr; /* & same as && */
+            }
+        }
+
+        ctx->idr = idr_access & !ctx->idr; /* & same as && */
+    }
+
     if (ctx->display_orientation != PASS) {
         for (i = au->nb_units - 1; i >= 0; i--) {
             H264RawSEI *sei;
@@ -658,6 +677,8 @@ static int h264_metadata_init(AVBSFContext *bsf)
     CodedBitstreamFragment *au = &ctx->access_unit;
     int err, i;
 
+    ctx->idr = 2 * !ctx->idr;
+
     err = ff_cbs_init(&ctx->input,  AV_CODEC_ID_H264, bsf);
     if (err < 0)
         return err;
@@ -793,6 +814,9 @@ static const AVOption h264_metadata_options[] = {
     { "vertical",   "Set ver_flip",
         0, AV_OPT_TYPE_CONST,
         { .i64 = FLIP_VERTICAL },   .flags = FLAGS, .unit = "flip" },
+
+    { "idr", "Minimize idr_pic_id",
+        OFFSET(idr), AV_OPT_TYPE_INT, { .i64 = 1 }, 0, 1, FLAGS},
 
     { "level", "Set level (table A-1)",
         OFFSET(level), AV_OPT_TYPE_INT,
