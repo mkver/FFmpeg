mkver/FFmpeg

commit 935ec5a1d5841cefa6007d53a8a7e5968f4fb043

matroskadec: move buffer decoding code to a separate function original patch by Evgeniy Stepanov  eugeni.stepanov _at_ gmail.com
Originally committed as revision 13891 to svn://svn.ffmpeg.org/ffmpeg/trunk

libavformat/matroskadec.c
@@ -973,6 +973,82 @@ matroska_parse_info (MatroskaDemuxContext *matroska)
 }
 
 static int
+matroska_decode_buffer(uint8_t** buf, int* buf_size, MatroskaTrack *track)
+{
+    uint8_t* data = *buf;
+    int isize = *buf_size;
+    uint8_t* pkt_data = NULL;
+    int pkt_size = isize;
+    int result = 0;
+    int olen;
+
+    switch (track->encoding_algo) {
+    case MATROSKA_TRACK_ENCODING_COMP_HEADERSTRIP:
+        return track->encoding_settings_len;
+    case MATROSKA_TRACK_ENCODING_COMP_LZO:
+        do {
+            olen = pkt_size *= 3;
+            pkt_data = av_realloc(pkt_data,
+                                  pkt_size+LZO_OUTPUT_PADDING);
+            result = lzo1x_decode(pkt_data, &olen, data, &isize);
+        } while (result==LZO_OUTPUT_FULL && pkt_size<10000000);
+        if (result)
+            goto failed;
+        pkt_size -= olen;
+        break;
+#ifdef CONFIG_ZLIB
+    case MATROSKA_TRACK_ENCODING_COMP_ZLIB: {
+        z_stream zstream = {0};
+        if (inflateInit(&zstream) != Z_OK)
+            return -1;
+        zstream.next_in = data;
+        zstream.avail_in = isize;
+        do {
+            pkt_size *= 3;
+            pkt_data = av_realloc(pkt_data, pkt_size);
+            zstream.avail_out = pkt_size - zstream.total_out;
+            zstream.next_out = pkt_data + zstream.total_out;
+            result = inflate(&zstream, Z_NO_FLUSH);
+        } while (result==Z_OK && pkt_size<10000000);
+        pkt_size = zstream.total_out;
+        inflateEnd(&zstream);
+        if (result != Z_STREAM_END)
+            goto failed;
+        break;
+    }
+#endif
+#ifdef CONFIG_BZLIB
+    case MATROSKA_TRACK_ENCODING_COMP_BZLIB: {
+        bz_stream bzstream = {0};
+        if (BZ2_bzDecompressInit(&bzstream, 0, 0) != BZ_OK)
+            return -1;
+        bzstream.next_in = data;
+        bzstream.avail_in = isize;
+        do {
+            pkt_size *= 3;
+            pkt_data = av_realloc(pkt_data, pkt_size);
+            bzstream.avail_out = pkt_size - bzstream.total_out_lo32;
+            bzstream.next_out = pkt_data + bzstream.total_out_lo32;
+            result = BZ2_bzDecompress(&bzstream);
+        } while (result==BZ_OK && pkt_size<10000000);
+        pkt_size = bzstream.total_out_lo32;
+        BZ2_bzDecompressEnd(&bzstream);
+        if (result != BZ_STREAM_END)
+            goto failed;
+        break;
+    }
+#endif
+    }
+
+    *buf = pkt_data;
+    *buf_size = pkt_size;
+    return 0;
+ failed:
+    av_free(pkt_data);
+    return -1;
+}
+
+static int
 matroska_add_stream (MatroskaDemuxContext *matroska)
 {
     int res = 0;
@@ -2801,78 +2877,14 @@ matroska_parse_block(MatroskaDemuxContext *matroska, uint8_t *data, int size,
                     matroska_queue_packet(matroska, pkt);
                 }
             } else {
-                int result, offset = 0, ilen, olen, pkt_size = lace_size[n];
+                int offset = 0, pkt_size = lace_size[n];
                 uint8_t *pkt_data = data;
 
                 if (matroska->tracks[track]->encoding_scope & 1) {
-                    switch (matroska->tracks[track]->encoding_algo) {
-                    case MATROSKA_TRACK_ENCODING_COMP_HEADERSTRIP:
-                        offset = matroska->tracks[track]->encoding_settings_len;
-                        break;
-                    case MATROSKA_TRACK_ENCODING_COMP_LZO:
-                        pkt_data = NULL;
-                        do {
-                            ilen = lace_size[n];
-                            olen = pkt_size *= 3;
-                            pkt_data = av_realloc(pkt_data,
-                                                  pkt_size+LZO_OUTPUT_PADDING);
-                            result = lzo1x_decode(pkt_data, &olen, data, &ilen);
-                        } while (result==LZO_OUTPUT_FULL && pkt_size<10000000);
-                        if (result) {
-                            av_free(pkt_data);
-                            continue;
-                        }
-                        pkt_size -= olen;
-                        break;
-#ifdef CONFIG_ZLIB
-                    case MATROSKA_TRACK_ENCODING_COMP_ZLIB: {
-                        z_stream zstream = {0};
-                        pkt_data = NULL;
-                        if (inflateInit(&zstream) != Z_OK)
-                            continue;
-                        zstream.next_in = data;
-                        zstream.avail_in = lace_size[n];
-                        do {
-                            pkt_size *= 3;
-                            pkt_data = av_realloc(pkt_data, pkt_size);
-                            zstream.avail_out = pkt_size - zstream.total_out;
-                            zstream.next_out = pkt_data + zstream.total_out;
-                            result = inflate(&zstream, Z_NO_FLUSH);
-                        } while (result==Z_OK && pkt_size<10000000);
-                        pkt_size = zstream.total_out;
-                        inflateEnd(&zstream);
-                        if (result != Z_STREAM_END) {
-                            av_free(pkt_data);
-                            continue;
-                        }
-                        break;
-                    }
-#endif
-#ifdef CONFIG_BZLIB
-                    case MATROSKA_TRACK_ENCODING_COMP_BZLIB: {
-                        bz_stream bzstream = {0};
-                        pkt_data = NULL;
-                        if (BZ2_bzDecompressInit(&bzstream, 0, 0) != BZ_OK)
-                            continue;
-                        bzstream.next_in = data;
-                        bzstream.avail_in = lace_size[n];
-                        do {
-                            pkt_size *= 3;
-                            pkt_data = av_realloc(pkt_data, pkt_size);
-                            bzstream.avail_out = pkt_size - bzstream.total_out_lo32;
-                            bzstream.next_out = pkt_data + bzstream.total_out_lo32;
-                            result = BZ2_bzDecompress(&bzstream);
-                        } while (result==BZ_OK && pkt_size<10000000);
-                        pkt_size = bzstream.total_out_lo32;
-                        BZ2_bzDecompressEnd(&bzstream);
-                        if (result != BZ_STREAM_END) {
-                            av_free(pkt_data);
-                            continue;
-                        }
-                        break;
-                    }
-#endif
-                    }
+                    offset = matroska_decode_buffer(&pkt_data, &pkt_size,
+                                                    matroska->tracks[track]);
+                    if (offset < 0)
+                        continue;
                 }
 
                 pkt = av_mallocz(sizeof(AVPacket));

commit 65d316a317dc8eff50d655dfb4a0d4ebf9e80776

avformat/matroskadec: Fix lzo decompression
When a Matroska Block is only stored in compressed form, the size of
the uncompressed block is not explicitly coded and therefore not known
before decompressing it. Therefore the demuxer uses a guess for the
uncompressed size: The first guess is three times the compressed size
and if this is not enough, it is repeatedly incremented by a factor of
three. But when this happens with lzo, the decompression is neither
resumed nor started again. Instead when av_lzo1x_decode indicates that x
bytes of input data could not be decoded, because the output buffer is
already full, the first (not the last) x bytes of the input buffer are
resent for decoding in the next try; they overwrite already decoded
data.

This commit fixes this by instead restarting the decompression anew,
just with a bigger buffer.

This seems to be a regression since 935ec5a1.

A FATE-test for this has been added.

Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@gmail.com>

libavformat/matroskadec.c
@@ -1599,6 +1599,7 @@ static int matroska_decode_buffer(uint8_t **buf, int *buf_size,
 #if CONFIG_LZO
     case MATROSKA_TRACK_ENCODING_COMP_LZO:
         do {
+            int insize = isize;
             olen       = pkt_size *= 3;
             newpktdata = av_realloc(pkt_data, pkt_size + AV_LZO_OUTPUT_PADDING
                                                        + AV_INPUT_BUFFER_PADDING_SIZE);
@@ -1607,7 +1608,7 @@ static int matroska_decode_buffer(uint8_t **buf, int *buf_size,
                 goto failed;
             }
             pkt_data = newpktdata;
-            result   = av_lzo1x_decode(pkt_data, &olen, data, &isize);
+            result   = av_lzo1x_decode(pkt_data, &olen, data, &insize);
         } while (result == AV_LZO_OUTPUT_FULL && pkt_size < 10000000);
         if (result) {
             result = AVERROR_INVALIDDATA;

tests/fate/matroska.mak
@@ -22,6 +22,10 @@ fate-matroska-xiph-lacing: CMD = framecrc -i $(TARGET_SAMPLES)/mkv/xiph_lacing.m
 FATE_MATROSKA-$(call ALLYES, MATROSKA_DEMUXER ZLIB) += fate-matroska-zlib-decompression
 fate-matroska-zlib-decompression: CMD = framecrc -i $(TARGET_SAMPLES)/mkv/subtitle_zlib.mks -c:s copy
 
+# This tests that the matroska demuxer can decompress lzo compressed tracks.
+FATE_MATROSKA-$(call ALLYES, MATROSKA_DEMUXER LZO) += fate-matroska-lzo-decompression
+fate-matroska-lzo-decompression: CMD = framecrc -i $(TARGET_SAMPLES)/mkv/lzo.mka -c copy
+
 # This tests that the matroska demuxer correctly propagates
 # the channel layout contained in vorbis comments in the CodecPrivate
 # of flac tracks. It also tests header removal compression.

tests/ref/fate/matroska-lzo-decompression
@@ -1,0 +1,10 @@
+#tb 0: 11337/500000000
+#media_type 0: audio
+#codec_id 0: pcm_s16le
+#sample_rate 0: 44100
+#channel_layout 0: 3
+#channel_layout_name 0: stereo
+0,          0,          0,     4096,    16384, 0x00000000
+0,       4096,       4096,     4096,    16384, 0xad7eebf4
+0,       8192,       8192,     4096,    16384, 0x1d1ff9d9
+0,      12288,      12288,     4097,    16384, 0xf1d9e2e2