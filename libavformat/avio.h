ac9df43d89c97963a1eea058e1a90ca94e68d9ae
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-07-30 14:20:23 +0200

    avformat/avio: Don't include common.h
    Also remove an unnecessary version.h inclusion while at it.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavformat/avio.h
+++ libavformat/avio.h
@@ -27,12 +27,11 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
-#include "libavutil/common.h"
+#include "libavutil/attributes.h"
 #include "libavutil/dict.h"
 #include "libavutil/log.h"
-
-#include "libavformat/version.h"
 
 /**
  * Seeking works like for a local file.
