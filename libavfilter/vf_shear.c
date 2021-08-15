0f250b74ac80ca9c9b4e0b4c893d7bb87ba8966a
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-08-16 01:04:21 +0200

    avfilter/vf_shear: Free input frames generically
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_shear.c
+++ libavfilter/vf_shear.c
@@ -231,10 +231,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     AVFrame *out;
 
     out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
-    if (!out) {
-        av_frame_free(&in);
+    if (!out)
         return AVERROR(ENOMEM);
-    }
     av_frame_copy_props(out, in);
 
     /* fill background */
@@ -246,7 +244,6 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     ff_filter_execute(ctx, s->filter_slice[s->interp], &td, NULL,
                       FFMIN(s->planeheight[1], ff_filter_get_nb_threads(ctx)));
 
-    av_frame_free(&in);
     return ff_filter_frame(outlink, out);
 }
 
@@ -300,6 +297,7 @@ static const AVFilterPad inputs[] = {
     {
         .name         = "default",
         .type         = AVMEDIA_TYPE_VIDEO,
+        .flags        = AVFILTERPAD_FLAG_GENERIC_FREE,
         .filter_frame = filter_frame,
     },
 };
