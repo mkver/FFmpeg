cf99fdac7f0e63f91768c336d796c2216504fe3a
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-08-16 01:04:21 +0200

    avfilter/vf_hqx: Free input frames generically
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_hqx.c
+++ libavfilter/vf_hqx.c
@@ -488,10 +488,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     HQXContext *hqx = ctx->priv;
     ThreadData td;
     AVFrame *out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
-    if (!out) {
-        av_frame_free(&in);
+    if (!out)
         return AVERROR(ENOMEM);
-    }
     av_frame_copy_props(out, in);
     out->width  = outlink->w;
     out->height = outlink->h;
@@ -502,7 +500,6 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     ff_filter_execute(ctx, hqx->func, &td, NULL,
                       FFMIN(inlink->h, ff_filter_get_nb_threads(ctx)));
 
-    av_frame_free(&in);
     return ff_filter_frame(outlink, out);
 }
 
@@ -537,6 +534,7 @@ static const AVFilterPad hqx_inputs[] = {
     {
         .name         = "default",
         .type         = AVMEDIA_TYPE_VIDEO,
+        .flags        = AVFILTERPAD_FLAG_GENERIC_FREE,
         .filter_frame = filter_frame,
     },
 };
