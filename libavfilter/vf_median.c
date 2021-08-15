dd5898fe5425235afe7647fcf7f04c0d974577ce
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-08-16 01:04:21 +0200

    avfilter/vf_median: Free input frames generically
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_median.c
+++ libavfilter/vf_median.c
@@ -223,16 +223,13 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     AVFrame *out;
 
     out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
-    if (!out) {
-        av_frame_free(&in);
+    if (!out)
         return AVERROR(ENOMEM);
-    }
     av_frame_copy_props(out, in);
 
     td.in = in; td.out = out;
     ff_filter_execute(ctx, filter_slice, &td, NULL, s->nb_threads);
 
-    av_frame_free(&in);
     return ff_filter_frame(outlink, out);
 }
 
@@ -270,6 +267,7 @@ static const AVFilterPad median_inputs[] = {
     {
         .name         = "default",
         .type         = AVMEDIA_TYPE_VIDEO,
+        .flags        = AVFILTERPAD_FLAG_GENERIC_FREE,
         .config_props = config_input,
         .filter_frame = filter_frame,
     },
