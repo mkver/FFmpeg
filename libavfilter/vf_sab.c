30e1cd63aa863f8a92366fd4a3cd0070e97cf199
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-08-16 01:04:21 +0200

    avfilter/vf_sab: Free input frames generically
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_sab.c
+++ libavfilter/vf_sab.c
@@ -283,10 +283,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *inpic)
     AVFrame *outpic;
 
     outpic = ff_get_video_buffer(outlink, outlink->w, outlink->h);
-    if (!outpic) {
-        av_frame_free(&inpic);
+    if (!outpic)
         return AVERROR(ENOMEM);
-    }
     av_frame_copy_props(outpic, inpic);
 
     blur(outpic->data[0], outpic->linesize[0], inpic->data[0],  inpic->linesize[0],
@@ -298,7 +296,6 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *inpic)
         blur(outpic->data[2], outpic->linesize[2], inpic->data[2], inpic->linesize[2], cw, ch, &s->chroma);
     }
 
-    av_frame_free(&inpic);
     return ff_filter_frame(outlink, outpic);
 }
 
@@ -306,6 +303,7 @@ static const AVFilterPad sab_inputs[] = {
     {
         .name         = "default",
         .type         = AVMEDIA_TYPE_VIDEO,
+        .flags        = AVFILTERPAD_FLAG_GENERIC_FREE,
         .filter_frame = filter_frame,
         .config_props = config_props,
     },
