e18d8ba0e044881f2a18863b6bcb323be5426403
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2021-08-16 01:04:21 +0200

    avfilter/vf_rotate: Free input frames generically
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavfilter/vf_rotate.c
+++ libavfilter/vf_rotate.c
@@ -512,10 +512,8 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
     double res;
 
     out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
-    if (!out) {
-        av_frame_free(&in);
+    if (!out)
         return AVERROR(ENOMEM);
-    }
     av_frame_copy_props(out, in);
 
     rot->var_values[VAR_N] = inlink->frame_count_out;
@@ -552,7 +550,6 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
                           FFMIN(outh, ff_filter_get_nb_threads(ctx)));
     }
 
-    av_frame_free(&in);
     return ff_filter_frame(outlink, out);
 }
 
@@ -583,6 +580,7 @@ static const AVFilterPad rotate_inputs[] = {
     {
         .name         = "default",
         .type         = AVMEDIA_TYPE_VIDEO,
+        .flags        = AVFILTERPAD_FLAG_GENERIC_FREE,
         .filter_frame = filter_frame,
     },
 };
