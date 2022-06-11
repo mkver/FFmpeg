61f28f13c0c431ed76ac66f680fb23a56c7dc409
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-06-11 16:24:23 +0200

    avcodec/x86/h264dsp_init: Remove obsolete MMX(EXT) functions
    x64 always has MMX, MMXEXT, SSE and SSE2 and this means
    that some functions for MMX, MMXEXT and 3dnow are always
    overridden by other functions (unless one e.g. explicitly
    disables SSE2) for x64. So given that the only systems that
    benefit from these functions are truely ancient 32bit x86s
    they are removed.
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/x86/h264_deblock_10bit.asm
+++ libavcodec/x86/h264_deblock_10bit.asm
@@ -798,9 +798,11 @@ cglobal deblock_h_luma_intra_10, 4,7,8*(mmsize/16)
 %endmacro
 
 %if ARCH_X86_64 == 0
+%if HAVE_ALIGNED_STACK == 0
 INIT_MMX mmxext
 DEBLOCK_LUMA
 DEBLOCK_LUMA_INTRA
+%endif
 INIT_XMM sse2
 DEBLOCK_LUMA
 DEBLOCK_LUMA_INTRA
@@ -1068,10 +1070,6 @@ RET
 
 %endmacro
 
-%if ARCH_X86_64 == 0
-INIT_MMX mmxext
-DEBLOCK_CHROMA
-%endif
 INIT_XMM sse2
 DEBLOCK_CHROMA
 %if HAVE_AVX_EXTERNAL
