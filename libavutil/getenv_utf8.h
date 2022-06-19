00560531acd387ab09c1cf53963f3a05eccdc8e3
Nil Admirari <nil-admirari@mailo.com>
2022-06-19 14:40:52 +0300

    libavutil: Add wchartoutf8(), wchartoansi(), utf8toansi(), getenv_utf8() and freeenv_utf8()
    wchartoutf8() converts strings returned by WinAPI into UTF-8,
    which is FFmpeg's preffered encoding.
    
    Some external dependencies, such as AviSynth, are still
    not Unicode-enabled. utf8toansi() converts UTF-8 strings
    into ANSI in two steps: UTF-8 -> wchar_t -> ANSI.
    wchartoansi() is responsible for the second step of the conversion.
    Conversion in just one step is not supported by WinAPI.
    
    Since these character converting functions allocate the buffer
    of necessary size, they also facilitate the removal of MAX_PATH limit
    in places where fixed-size ANSI/WCHAR strings were used
    as filename buffers.
    
    getenv_utf8() wraps _wgetenv() converting its input from
    and its output to UTF-8. Compared to plain getenv(),
    getenv_utf8() requires a cleanup.
    
    Because of that, in places that only test the existence of
    an environment variable or compare its value with a string
    consisting entirely of ASCII characters, the use of plain getenv()
    is still preferred. (libavutil/log.c check_color_terminal()
    is an example of such a place.)
    
    Plain getenv() is also preffered in UNIX-only code,
    such as bktr.c, fbdev_common.c, oss.c in libavdevice
    or af_ladspa.c in libavfilter.

---
+++ libavutil/getenv_utf8.h
@@ -0,0 +1,83 @@
+/*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVUTIL_GETENV_UTF8_H
+#define AVUTIL_GETENV_UTF8_H
+
+#include <stdlib.h>
+
+#include "config.h"
+#include "mem.h"
+
+#if HAVE_GETENV
+
+#ifdef _WIN32
+
+#include "libavutil/wchar_filename.h"
+
+static inline char *getenv_utf8(const char *varname)
+{
+    wchar_t *varname_w, *var_w;
+    char *var;
+
+    if (utf8towchar(varname, &varname_w))
+        return NULL;
+    if (!varname_w)
+        return NULL;
+
+    var_w = _wgetenv(varname_w);
+    av_free(varname_w);
+
+    if (!var_w)
+        return NULL;
+    if (wchartoutf8(var_w, &var))
+        return NULL;
+
+    return var;
+
+    // No CP_ACP fallback compared to other *_utf8() functions:
+    // non UTF-8 strings must not be returned.
+}
+
+static inline void freeenv_utf8(char *var)
+{
+    av_free(var);
+}
+
+#else
+
+static inline char *getenv_utf8(const char *varname)
+{
+    return getenv(varname);
+}
+
+static inline void freeenv_utf8(char *var)
+{
+}
+
+#endif // _WIN32
+
+#else
+
+#define getenv_utf8(x) NULL
+
+#define freeenv_utf8(x) ((void) 0)
+
+#endif // HAVE_GETENV
+
+#endif // AVUTIL_GETENV_UTF8_H
