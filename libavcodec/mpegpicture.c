c0fa9d59403b443c1fa6990bb148ffb75230c897
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-08-04 17:18:18 +0200

    avcodec/mpegpicture: Use refstruct-API for hwaccel_picture_private
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/mpegpicture.c
+++ libavcodec/mpegpicture.c
@@ -30,6 +30,7 @@
 #include "motion_est.h"
 #include "mpegpicture.h"
 #include "mpegutils.h"
+#include "refstruct.h"
 #include "threadframe.h"
 
 static void av_noinline free_picture_tables(Picture *pic)
@@ -135,12 +136,11 @@ static int alloc_frame_buffer(AVCodecContext *avctx,  Picture *pic,
     if (avctx->hwaccel) {
         assert(!pic->hwaccel_picture_private);
         if (avctx->hwaccel->frame_priv_data_size) {
-            pic->hwaccel_priv_buf = av_buffer_allocz(avctx->hwaccel->frame_priv_data_size);
-            if (!pic->hwaccel_priv_buf) {
+            pic->hwaccel_picture_private = ff_refstruct_allocz(avctx->hwaccel->frame_priv_data_size);
+            if (!pic->hwaccel_picture_private) {
                 av_log(avctx, AV_LOG_ERROR, "alloc_frame_buffer() failed (hwaccel private data allocation)\n");
                 return -1;
             }
-            pic->hwaccel_picture_private = pic->hwaccel_priv_buf->data;
         }
     }
 
@@ -260,7 +260,7 @@ fail:
  */
 void ff_mpeg_unref_picture(AVCodecContext *avctx, Picture *pic)
 {
-    int off = offsetof(Picture, hwaccel_priv_buf) + sizeof(pic->hwaccel_priv_buf);
+    int off = offsetof(Picture, hwaccel_picture_private) + sizeof(pic->hwaccel_picture_private);
 
     pic->tf.f = pic->f;
     /* WM Image / Screen codecs allocate internal buffers with different
@@ -272,7 +272,7 @@ void ff_mpeg_unref_picture(AVCodecContext *avctx, Picture *pic)
     else if (pic->f)
         av_frame_unref(pic->f);
 
-    av_buffer_unref(&pic->hwaccel_priv_buf);
+    ff_refstruct_unref(&pic->hwaccel_picture_private);
 
     free_picture_tables(pic);
 
@@ -328,14 +328,8 @@ int ff_mpeg_ref_picture(AVCodecContext *avctx, Picture *dst, Picture *src)
     if (ret < 0)
         goto fail;
 
-    if (src->hwaccel_picture_private) {
-        dst->hwaccel_priv_buf = av_buffer_ref(src->hwaccel_priv_buf);
-        if (!dst->hwaccel_priv_buf) {
-            ret = AVERROR(ENOMEM);
-            goto fail;
-        }
-        dst->hwaccel_picture_private = dst->hwaccel_priv_buf->data;
-    }
+    ff_refstruct_replace(&dst->hwaccel_picture_private,
+                          src->hwaccel_picture_private);
 
     dst->field_picture           = src->field_picture;
     dst->b_frame_score           = src->b_frame_score;
