11fac85e7ed7c078a6e0a936c98a9d61dc8e158e
Andreas Rheinhardt <andreas.rheinhardt@outlook.com>
2022-08-23 12:03:53 +0200

    avcodec/get_buffer: Check for correct HW pixel format
    The documentation for the get_buffer2-callback contains
    the clause that format, width and height of the frame
    may differ from the corresponding values from the
    AVCodecContext and that the frame's values are to be used.
    
    (One should probably also add a check for width and height,
    but given that they are already overridden afterwards, it
    is unclear what to do exactly.)
    
    Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

--- libavcodec/get_buffer.c
+++ libavcodec/get_buffer.c
@@ -284,10 +284,13 @@ int avcodec_default_get_buffer2(AVCodecContext *avctx, AVFrame *frame, int flags
     int ret;
 
     if (avctx->hw_frames_ctx) {
-        ret = av_hwframe_get_buffer(avctx->hw_frames_ctx, frame, 0);
-        frame->width  = avctx->coded_width;
-        frame->height = avctx->coded_height;
-        return ret;
+        const AVHWFramesContext *hwctx = (AVHWFramesContext*)avctx->hw_frames_ctx->data;
+        if (hwctx->format == frame->format) {
+            ret = av_hwframe_get_buffer(avctx->hw_frames_ctx, frame, 0);
+            frame->width  = avctx->coded_width;
+            frame->height = avctx->coded_height;
+            return ret;
+        }
     }
 
     if ((ret = update_frame_pool(avctx, frame)) < 0)
